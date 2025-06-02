20143c3bf707d45a68d695c47bf5f2e2165f9dd7
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-06-02 01:48:54 +0100

    added the authentification systeme using the service design pattern

---
+++ orchestrify/app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Http\Controllers;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller {
+
+    protected $authService; 
+
+    public function __construct(AuthService $authService){
+        $this->authService = $authService;
+    }
+
+
+    public function showRegisterForm(){
+        return view('register');
+    }
+
+    public function register(Request $request){
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user= $this->authService->register([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+        ]);
+
+        Auth::login($user); 
+        session()->flash('success', 'User registered successfully!');
+        return redirect()->route('login.form');
+    }
+
+
+
+        public function showLoginForm()
+    {
+        return view('login');
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        
+        $user = $this->authService->login($credentials['email'], $credentials['password']);
+
+        if(!auth()->user()->profile_completed){
+            return redirect(route('profile.show'));
+        }
+
+        return redirect()->route('profile.index');
+    }   
+
+
+       public function logout()
+    {
+        $this->authService->logout();
+        
+        Auth::logout();
+        session()->flash('success', 'Logout successful!');
+        return redirect()->route('login');
+    }
+
+
+}
---
+++ orchestrify/app/services/AuthService.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Services;
+use App\Models\User;
+use Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService {
+
+    public function register (array $data): User{
+        $user = User::create([
+            'name' => $data['name'], 
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $user;
+
+    }
+
+
+
+    public function login ($email , $password){
+
+        if(Auth::attempt(['email' => $email, 'password' => $password])){
+            return false; 
+        }
+
+        $user = User::where('email', $email)->first();
+        $user->profile_completed =true ; 
+        $user->save(); 
+
+        return $user ; 
+    }
+
+
+    public function logout(){
+       $user = Auth::user();
+        if(!$user){
+            return false ; 
+        }
+
+        $user->tokens()->delete(); 
+        return true ; 
+    }
+}
